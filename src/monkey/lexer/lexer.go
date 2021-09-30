c1d47fe5ea05bb87d6ee752f71e80a551a0d754b
john <johnrin1987@gmail.com>
2021-09-29 19:04:22 -0500

    updated token and lex to more reliably scan

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -15,6 +15,19 @@ func New(input string) *Lexer {
 	return l
 }
 
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string{
+	position := l.position
+	for isLetter(l.ch){
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -30,8 +43,29 @@ func newToken(tokenType token.TokenType, ch byte) token.Token{
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+func (l *Lexer) skipWhiteSpace(){
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r'{
+		//simply advance the reader
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string{
+	position := l.position
+	for isDigit(l.ch){
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool{
+	return '0' <= ch && ch <= '9'
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhiteSpace()
 
 	switch l.ch {
 	case '=':
@@ -53,6 +87,18 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch){
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}else if isDigit(l.ch){
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}else{
+			tok = newToken(token.ILLEGAL,l.ch)
+		}
 
 	}
 	l.readChar()
